b8e8322e53542d43cd90bb5bdc3ba7add6890836
azfarmasood <azfarmasood22@gmail.com>
2023-12-10 01:18:40 +0500

    deleat

--- 00.hackathon_one/next-app/src/components/shared/Client/Tools/DarkThemeMode/DarkThememode.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { FC } from "react";
-import { ThemeProvider } from "next-themes";
-const DarkThememode: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
-};
-
-export default DarkThememode;
