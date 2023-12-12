8e2f8a3ca8074ef43e67302cf0ca5878db3ec4a6
azfarmasood <azfarmasood22@gmail.com>
2023-12-12 16:56:05 +0500

    deleated

--- 00.hackathon_one/next-app/src/app/favourites/page.tsx
+++
@@ -1,9 +0,0 @@
-import Favourites_Components from "@/components/widgets/Favourites_Components";
-
-export default function Favourites () {
-    return (
-      <main>
-          <Favourites_Components />
-      </main>
-    );
-}
